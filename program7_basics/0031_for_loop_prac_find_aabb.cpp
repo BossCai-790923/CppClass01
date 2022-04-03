5afb70b95caaba1ce233d22fa0f0cd5bb4c6ccce
Tom Fan <tomboxfan@hotmail.com>
2022-04-03 10:21:26 +0800

    lesson 73

---
+++ program7_basics/0031_for_loop_prac_find_aabb.cpp
@@ -0,0 +1,13 @@
+/*
+--------------
+Requirement
+--------------
+Use 2 solutions to find all 4 digit square numbers in format like aabb.
+
+
+if(sqrt(n) == floor(sqrt(n)))
+    printf("%d\n", n);
+
+
+
+*/
---
+++ program7_basics/0041_while_loop_prac_1.cpp
@@ -0,0 +1,18 @@
+/*
+
+Homework
+--------------
+Requirement
+--------------
+For any integer n
+if it is odd, change it to 3n+1
+if it is even, change it to n/2
+
+Change n until n becomes 1,
+Print how many changes times it requires.
+
+
+Condition:
+n <= 10^9
+
+*/
