c947237f141f7d9994752042be535b6dc38f59bf
Tom Fan <tomboxfan@hotmail.com>
2022-04-10 10:15:36 +0800

    lesson 75

--- program7_basics/0031_for_loop_prac_find_aabb.cpp
+++ program7_basics/0031_for_loop_prac_find_aabb.cpp
@@ -11,3 +11,20 @@ if(sqrt(n) == floor(sqrt(n)))
 
 
 */
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    for(int i = 1; i <= 9; i++ ) 
+    for(int j = 0; j <= 9; j++) {
+        int n = i * 1000 + i * 100 + j * 10 + j;
+        if(sqrt(n) == floor(sqrt(n))) printf("%d\n", n);
+    }
+
+
+    return 0;
+}
--- program7_basics/0041_while_loop_prac_1.cpp
+++ program7_basics/0041_while_loop_prac_1.cpp
@@ -16,3 +16,71 @@ Condition:
 n <= 10^9
 
 */
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    // int n, count = 0;
+    // scanf("%d", &n);
+
+    long long n;
+    int count = 0;
+    scanf("%lld", &n);
+
+
+    while(n > 1) {
+        if(n % 2 == 1)
+            n = n * 3 + 1;
+        else
+            n /= 2;
+
+        count++;
+    }
+
+    printf("%d\n", count);
+
+    return 0;
+}
+
+
+/*
+IMPORTANTCE!!! ------------------------------------------
+1) int variable takes 4 bytes, which is 32 bits.
+2) 1st bit is used a sign. 0 means positive number / 1 means negative number
+3) 31 remaining bits are used to store a binary number, ranging from 0.....0 to 1......1
+   convert the binary number to decimal number, ranging from 0 to 2^31-1
+   1   -> 2^1-1 = 1
+   11  -> 2^2-1 = 3
+   111 -> 2^3-1 = 7
+   ...
+   11...1 -> 2^31-1 
+
+4) consider the sign bit in the front, so
+   int var range is [-2^31, 2^31-1]
+   2^3 = 8
+   2^10 = 1024  equals to 10^3
+   2^30 == 10^9
+   2^31 == 2*10^9
+   [-2,000,000,000      ,   2,000,000,000 ]
+
+int var range is negative 2 billion to positive 2 billion
+
+
+5) C/C++ has a data type called 'long long', which takes 8 bytes, which is 64 bits. 
+
+[HOMEWORK] tell me the range of 'long long'
+
+
+
+
+---------------------------------------------------------
+
+
+
+
+
+*/
