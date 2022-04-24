e05d2d9914e556428f3d9f8934fc397b32dd6e98
Tom Fan <tomboxfan@hotmail.com>
2022-04-24 10:30:27 +0800

    lesson 77

---
+++ program7_basics/0052_prac_numbers.cpp
@@ -0,0 +1,24 @@
+/*
+Requirement:
+There are multiple lines of inputs.
+Each line contains 2 int numbers, n and m, n < m < 10^6
+Use freopen to store input in a file.
+
+Calculate:
+1/n^2 + 1/(n+1)^2 + .... + 1/m^2
+
+Sample:
+
+Input:
+2 4
+65536 655360
+
+Output:
+0.42361
+0.00001
+
+
+Condition:
+n <= 10^6
+
+*/
