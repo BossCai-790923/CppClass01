69559811c20f625c535fa0489e7f3a080d0732a6
Tom Fan <tomboxfan@hotmail.com>
2022-05-07 18:26:31 +0800

    lesson 78

--- program7_basics/0052_prac_numbers.cpp
+++ program7_basics/0052_prac_numbers.cpp
@@ -22,3 +22,43 @@ Condition:
 n <= 10^6
 
 */
+
+
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
+
+int main()
+{
+
+
+    freopen("0052_input.txt", "r", stdin);
+
+    int n, m;
+
+    while(scanf("%d%d", &n, &m) == 2) {
+
+        float result = 0;
+
+        for(int i=n; i<=m; i++) {
+            result += 1.0 / ((long long)i * i);
+        }
+
+        /*
+        1) Because i can be 10^6, so i^2 will exceed int range
+        so we have to cast the result to long long type
+
+        2) 1 /10 gives you a int result
+           1.0 / 10 gives you a float result
+           So, we have to use 1.0
+        */
+
+        printf("%.5f\n", result);
+
+    }
+
+
+
+
+    return 0;
+}
